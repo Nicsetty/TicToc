d426a93e1b1330dd37b8ae9029a20e92fbb5185d
nicsetty <nikshinzgames@gmail.com>
2024-08-30 20:32:19 +0530

    Initial commit for Tic-Toc project

---
+++ out/clang-release/txc1.o.d
@@ -0,0 +1,4 @@
+out/clang-release//txc1.o: txc1.cc \
+  C:/Users/nimsh/Downloads/omnetpp-6.0.2-windows-x86_64/omnetpp-6.0.2/include/omnetpp.h
+
+C:/Users/nimsh/Downloads/omnetpp-6.0.2-windows-x86_64/omnetpp-6.0.2/include/omnetpp.h:
